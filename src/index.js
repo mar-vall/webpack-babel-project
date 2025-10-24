19f60802220d265f60d902f39cf795287be4c1f3
Mariana Vallejos <marivalleandrea@gmail.com>
2025-10-23 21:35:16 -0400

    add webpack config file and scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import { message } from './message';
 import './styles.css';
+import logo from './assets/logo-assuresoft.png';
 
-const message = document.createElement('h1');
-message.textContent = "Hello Webpack!";
-document.body.appendChild(message);
+const root = document.createElement('div');
+root.innerHTML = `
+  <h1>${message}</h1>
+  <p>Bundled with <strong>Webpack</strong> and transpiled by <strong>Babel</strong>.</p>
+  <img src="${logo}" alt="Logo" />
+`;
+
+document.body.appendChild(root);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  mode: "development",
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+  },
+};
